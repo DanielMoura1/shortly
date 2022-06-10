71fe08a1b7536852629258f0fdc101eb7990ce85
Daniel <daniel.bnh13gmail.com>
2022-06-10 17:11:39 -0300

    ??1

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+import  'dotenv/config'
 import express from 'express';
 import pg from 'pg';
 
 const { Pool } = pg;
 
 const connection = new Pool({
-  user: 'postgres',
+  user: process.env.user,
   host: 'localhost',
   port: 5432, 
   database: 'shortly',
-  password: ''
+  password: process.env.senha
 });
 
 const app = express();
